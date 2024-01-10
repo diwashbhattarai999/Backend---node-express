3143f49118908c167c827e204b752ac862f1430b
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-10 19:22:11 +0545

    feat: basic intro to backend using express

---
+++ day-1/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+// const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("diwashb999");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login here!");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Youtube page</h2>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
