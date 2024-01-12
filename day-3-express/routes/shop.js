452628f70af11e9889f4fb6e6ad86a9e6f78fd9d
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-12 18:20:03 +0545

    feat: fix some styles & learn about sharing data across files

--- day-3-express/routes/shop.js
+++ day-3-express/routes/shop.js
@@ -3,11 +3,12 @@ const express = require("express");
 const path = require("path");
 
 const rootDir = require("../utils/path");
+const adminData = require("./admin.js");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  //   console.log("In the another middleware");
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
